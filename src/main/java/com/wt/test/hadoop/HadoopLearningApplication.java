f5d475cc927f50ea1451c341c531cdc4eed26861
qiyu <jsj0804wt@126.com>
2019-10-27 23:00:51 +0800

    创建工程 by qiyu 2019-Oct-27

---
+++ src/main/java/com/wt/test/hadoop/HadoopLearningApplication.java
@@ -0,0 +1,13 @@
+package com.wt.test.hadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HadoopLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wt/test/hadoop/HadoopLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.test.hadoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadoopLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
